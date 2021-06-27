ff3243755394ea814dee78f918375660670c3a24
parthdhake <parthdhake@gmail.com>
2021-06-27 18:28:43 +0530

    Connection Room

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-const io = require('socket.io')();
-
-io.on("connection" , (client) => {
+const io = require("socket.io")(4000 , {
+    cors: {
+        origin : ["http://localhost:3000"]
+    }
 })
 
-const port = 8000;
-io.listen(port);
-console.log('listening on port ', port);
+
+let roomid = []
+
+
+
+io.on("connection" , (client) => {
+    const room_id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
+    console.log(client.id)
+
+    roomid.push(room_id)
+    client.emit("create-game",room_id)
+
+    client.on("join-game", (id) => {
+        console.log(id);
+        client.join(id)
+        var valid = null
+        console.log(roomid);
+        if (roomid.includes(id)) {
+            valid = true
+        } else {
+            valid = false
+        }
+        client.emit("validate", valid)
+    })
+
+    client.on("testvalue" , (testvalue , room) => {
+        console.log(testvalue);
+        console.log(room);
+        client.to(room).emit("testvalue" , testvalue)
+    })
+
+})
+
