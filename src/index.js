fff2765c7ff7ca66e1c37c2313f000a0444b3e3e
PiyushIngale007 <ingalepiyush@gmail.com>
2021-06-30 18:18:24 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,55 +1,52 @@
-const io = require("socket.io")(4000 , {
-    cors: {
-        origin : ["http://localhost:3000"]
-    }
-})
+const io = require('socket.io')(4000, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
 
+let roomid = [];
 
-let roomid = []
+io.on('connection', (client) => {
+  const room_id = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
+  console.log(client.id);
 
+  client.on('create-game', (name) => {
+    roomid.push({ RoomId: room_id, Player1: name, Player2: null });
+  });
 
-io.on("connection" , (client) => {
-    const room_id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
-    console.log(client.id)
+  client.emit('create-game', room_id);
 
-    
-
-    client.on('create-game', (name) => {
-        roomid.push({RoomId:room_id,Player1:name,Player2:null});
+  client.on('join-game', (id, name) => {
+    console.log(id);
+    client.join(id);
+    let valid = null;
+    console.log(roomid);
+    let Room = null;
+    roomid.forEach((room) => {
+      if (room.RoomId === id) {
+        room.Player2 = name;
+        valid = true;
+        Room = room;
+        client.emit('validate', valid, Room);
+        client.to(Room.RoomId).emit('testvalue', Room);
+      } else {
+        valid = false;
+      }
     });
+    console.log(roomid);
+    // if (roomid.includes(id)) {
+    //     valid = true
+    // } else {
+    //     valid = false
+    // }
+  });
 
-    client.emit("create-game",room_id)
-
-    client.on("join-game", (id,name) => {
-        console.log(id);
-        client.join(id)
-        let valid = null
-        console.log(roomid);
-        let Room=null
-        roomid.forEach((room)=>{
-            if(room.RoomId === id){
-                room.Player2 = name
-                valid = true
-                Room = room
-            }
-            else{
-                valid = false
-            }
-        })
-        console.log(roomid);
-        // if (roomid.includes(id)) {
-        //     valid = true
-        // } else {
-        //     valid = false
-        // }
-        client.emit("validate", valid,Room)
-    })
-
-    client.on("testvalue" , (testvalue , room) => {
-        console.log(testvalue);
-        console.log(room);
-        client.to(room).emit("testvalue" , testvalue)
-    })
-
-})
-
+  client.on('testvalue', (testvalue, room) => {
+    console.log(testvalue);
+    console.log(room);
+    client.to(room).emit('testvalue', testvalue);
+  });
+});
