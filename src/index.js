6df94d6390a804f38e0eafe72f6a8c9552dbb0b8
parthdhake <parthdhake@gmail.com>
2021-07-01 13:03:49 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-const io = require('socket.io')(4000, {
+const io = require("socket.io")(4000, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: ["http://localhost:3000"],
   },
 });
 
 let roomid = [];
 
-io.on('connection', (client) => {
+io.on("connection", (client) => {
   const room_id = Math.random()
     .toString(36)
-    .replace(/[^a-z]+/g, '')
+    .replace(/[^a-z]+/g, "")
     .substr(0, 5);
   console.log(client.id);
 
-  client.on('create-game', (name) => {
-    roomid.push({ RoomId: room_id, Player1: name, Player2: null });
+  client.on("create-game", (name) => {
+    const Player1Piece = Math.random() > 0.5 ? "X" : "O";
+    const Player2Piece = Player1Piece === "X" ? "O" : "X";
+
+    roomid.push({
+      RoomId: room_id,
+      Player1: name,
+      Player2: null,
+      Player1Piece: Player1Piece,
+      Player2Piece: Player2Piece,
+    });
   });
 
-  client.emit('create-game', room_id);
+  client.emit("create-game", room_id);
 
-  client.on('join-game', (id, name) => {
+  client.on("join-game", (id, name) => {
     console.log(id);
     client.join(id);
     let valid = null;
@@ -30,8 +39,8 @@ io.on('connection', (client) => {
         room.Player2 = name;
         valid = true;
         Room = room;
-        client.emit('validate', valid, Room);
-        client.to(Room.RoomId).emit('testvalue', Room);
+        client.emit("validate", valid, Room);
+        client.to(Room.RoomId).emit("testvalue", Room);
       } else {
         valid = false;
       }
@@ -44,9 +53,14 @@ io.on('connection', (client) => {
     // }
   });
 
-  client.on('testvalue', (testvalue, room) => {
+  client.on("onDivClick", (number, ID, piece) => {
+    console.log(number, ID, piece);
+    client.to(ID).emit("draw", number, piece);
+  });
+
+  client.on("testvalue", (testvalue, room) => {
     console.log(testvalue);
     console.log(room);
-    client.to(room).emit('testvalue', testvalue);
+    client.to(room).emit("testvalue", testvalue);
   });
 });
